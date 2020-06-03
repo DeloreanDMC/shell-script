99d8b2c8f22dbf831e496216aa6f876b44a7d877
milovanov <milovanovkostantin@gmail.com>
2020-06-03 22:12:33 +0300

    Done

---
+++ cur-processes.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Author : Milovanov Konstantin
+# Script : 
+# лучше через UID, чем через whoami, так как имя пользователя запустившего процесс может быть алиасом или обрезано  
+echo Wake up $(whoami)
+echo The Matrix has you...
+echo Your UID = $UID
+ps aux > processes.txt
+grep $UID processes.txt > myprocesses.txt
+read
